00b348009551d97dd291dcd74ae37d437bbbed6f
teihenn <hm.hr.yossy@gmail.com>
2025-01-21 23:10:23 +0900

    Add triplet_sum_bruteforce.py

---
+++ ByteByteGo101/TwoPointers/2_TripletSum/20250112/triplet_sum_bruteforce.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+# Time: O(n^3)
+# Space: O(n^3)
+def triplet_sum(nums: List[int]) -> List[List[int]]:
+    nums.sort()
+    l = len(nums)
+    ret_set = set()
+    for i in range(l):
+        for j in range(i + 1, l):
+            for k in range(j + 1, l):
+                sum = nums[i] + nums[j] + nums[k]
+                if sum == 0:
+                    ret_set.add((nums[i], nums[j], nums[k]))
+    return [list(s) for s in ret_set]
