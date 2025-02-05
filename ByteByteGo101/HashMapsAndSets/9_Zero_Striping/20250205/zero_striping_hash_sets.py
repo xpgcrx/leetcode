e21110c5f7cb63d224afc1ba1bc1b18c8e868115
teihenn <hm.hr.yossy@gmail.com>
2025-02-05 21:58:17 +0900

    Add some comments to zero_striping_hash_sets.py

--- ByteByteGo101/HashMapsAndSets/9_Zero_Striping/20250205/zero_striping_hash_sets.py
+++ ByteByteGo101/HashMapsAndSets/9_Zero_Striping/20250205/zero_striping_hash_sets.py
@@ -8,6 +8,10 @@ def zero_striping(matrix: List[List[int]]) -> None:
     column_zeros = set()
     m = len(matrix)
     n = len(matrix[0])
+
+    # Pass 1: Traverse through the matrix to identify the rows and
+    # columns containing zeros and store their indexes in the
+    # appropriate hash sets.
     for r in range(m):
         for c in range(n):
             num = matrix[r][c]
@@ -15,8 +19,8 @@ def zero_striping(matrix: List[List[int]]) -> None:
                 row_zeros.add(r)
                 column_zeros.add(c)
 
-    # row_zeros = {1,4}
-    # column_zeros = {1,4}
+    # Pass 2: Set any cell in the matrix to zero if its row index is
+    # in 'row_zeros' or its column index is in 'column_zeros'.
     for r in range(m):
         for c in range(n):
             if r in row_zeros or c in column_zeros:
