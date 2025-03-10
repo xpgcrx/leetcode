62d3ff3ba7cf9276dc8644d4adea12d9954c0ad7
teihenn <hm.hr.yossy@gmail.com>
2025-03-10 23:18:15 +0900

    Add next_lexicographical_sequence.py

---
+++ ByteByteGo101/TwoPointers/6_NextLexicographicalSequence/20250310/next_lexicographical_sequence.py
@@ -0,0 +1,22 @@
+# Time: O(n)
+# Space: O(n)
+def next_lexicographical_sequence(s: str) -> str:
+    letters = list(s)
+    pivot = len(letters) - 2
+    while 0 <= pivot and letters[pivot] >= letters[pivot + 1]:
+        pivot -= 1
+
+    if pivot == -1:
+        return "".join(reversed(letters))
+
+    rightmost_successor = len(letters) - 1
+    while letters[rightmost_successor] <= letters[pivot]:
+        rightmost_successor -= 1
+
+    letters[pivot], letters[rightmost_successor] = (
+        letters[rightmost_successor],
+        letters[pivot],
+    )
+
+    letters[pivot + 1 :] = reversed(letters[pivot + 1 :])
+    return "".join(letters)
